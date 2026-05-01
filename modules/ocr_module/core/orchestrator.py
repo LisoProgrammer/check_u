faba26d11ce87b812fad9fb46d3dc165dc6c4a57
Lisandro Zapata <zlisandro5@gmail.com>
2026-05-01 08:45:36 -0500

    Estructura base del modulo de OCR y el de extracción de texto

---
+++ modules/ocr_module/core/orchestrator.py
@@ -0,0 +1,20 @@
+# core/orchestrator.py
+from loaders.pdf_loader import load_pdf
+from ocr.tesseract_engine import TesseractEngine
+
+class OCRPipeline:
+
+    def __init__(self):
+        self.ocr_engine = TesseractEngine()
+
+    def process(self, pdf_bytes: bytes):
+        images = load_pdf(pdf_bytes)
+
+        full_text = ""
+        for img in images:
+            text = self.ocr_engine.extract_text(img)
+            full_text += text
+
+        return {
+            "text": full_text
+        }
---
+++ modules/ocr_module/ocr/engine.py
@@ -0,0 +1,7 @@
+# ocr/engine.py
+from abc import ABC, abstractmethod
+
+class OCREngine(ABC):
+    @abstractmethod
+    def extract_text(self, image) -> str:
+        pass
