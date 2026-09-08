25f3d36ec7c84596ed59f1c2cad87afa775cf6f8
Lisandro Zapata <zlisandro5@gmail.com>
2026-09-08 06:31:26 -0500

    Hace una petición al servidor del RUI para obtener el nombre del estudiante a partir del numero de documento

---
+++ modules/check_id/rui/consultar.py
@@ -0,0 +1,40 @@
+import requests
+
+url = "https://ventanillasocial.dnp.gov.co/Home/ObtenerDatosRUI"
+pre_data = {
+    "id": "1043645839"
+}
+
+def consultar(pre_data):
+    response = requests.post(
+        url,
+        data={
+            "pNumDoc": pre_data["id"],
+            "pTipDoc": "3"
+        },
+        timeout=10
+    )
+
+    #print(response.status_code)
+    #print(response.headers)
+    data = response.json()
+    #print(response.text)
+    response_format = {
+        "message": "",
+        "success": "",
+        "data": {
+            "id": "",
+            "nombre_completo": ""
+        }
+    }
+    if data["ok"] is True:
+        response_format["message"] = "Se encontró información relacionada al documento suministrado"
+        response_format["success"] = True
+        response_format["data"]["id"] = pre_data["id"]
+        response_format["data"]["nombre_completo"] = data["nombre"]
+    else:
+        response_format["message"] = "NO se encontró información relacionada al documento suministrado"
+        response_format["success"] = False
+        response_format["data"]["id"] = pre_data["id"]
+    return response_format
+print(consultar(pre_data))
